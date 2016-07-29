9c0efad281ee3958b2d105352f8fbaffe5bfd62e
Bestora <dennis.rehbehn@stud.fh-flensburg.de>
2016-07-29 14:57:54 +0200

    #164 fix
    #164 InvalidResponseException potential fix

--- PokemonGo.RocketAPI/Extensions/HttpClientExtensions.cs
+++ PokemonGo.RocketAPI/Extensions/HttpClientExtensions.cs
@@ -17,33 +17,35 @@ namespace PokemonGo.RocketAPI.Extensions
         {
             //Encode payload and put in envelop, then send
             var data = request.ToByteString();
+            await Task.Delay(200);
             var result = await client.PostAsync(url, new ByteArrayContent(data.ToByteArray()));
-
+    
             //Decode message
             var responseData = await result.Content.ReadAsByteArrayAsync();
             var codedStream = new CodedInputStream(responseData);
             var decodedResponse = new Response();
             decodedResponse.MergeFrom(codedStream);
-
+    
             return decodedResponse;
         }
-
+    
         public static async Task<TResponsePayload> PostProtoPayload<TRequest, TResponsePayload>(this HttpClient client,
             string url, TRequest request) where TRequest : IMessage<TRequest>
             where TResponsePayload : IMessage<TResponsePayload>, new()
         {
             Logger.Write($"Requesting {typeof(TResponsePayload).Name}", LogLevel.Debug);
+            await Task.Delay(200);
             var response = await PostProto(client, url, request);
-
+    
             if (response.Payload.Count == 0)
                 throw new InvalidResponseException();
-
+    
             //Decode payload
             //todo: multi-payload support
             var payload = response.Payload[0];
             var parsedPayload = new TResponsePayload();
             parsedPayload.MergeFrom(payload);
-
+    
             return parsedPayload;
         }
     }
